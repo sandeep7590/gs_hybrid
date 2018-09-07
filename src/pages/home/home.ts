9ceda78528bbf814a4460c5335fc3e2514da397a
sandeep <sandeep@darkhorsesports.in>
2018-09-07 19:02:32 +0530

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,42 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+ name: any;
+ dob:number;
+ gender: any;
+ email:any;
+ mobileno: any;
+ 
+ registerCredentials = { name: '',dob:'', gender: '', email: '',mobileno:'' };
 
   constructor(public navCtrl: NavController) {
 
   }
 
-}
+  register() {
+    //    console.log(this.name);
+      //  console.log(this.dob);
+        //alert(this.name);
+
+         
+      this.auth.register(this.registerCredentials).subscribe(success => {
+        if (success) {
+          this.createSuccess = true;
+        //  this.showPopup("Success", "Account created.");
+        } else {
+         // this.showPopup("Error", "Problem creating account.");
+        }
+      },
+        error => {
+          alert("Error", error);
+        });
+    }
+    
+
+    }
